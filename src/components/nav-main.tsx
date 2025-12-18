329e5a32ba6e1f6c5db2f2b6dadc5847da175c5b
Peter3Khalil <peter.khalil.fahmy@gmail.com>
2025-12-18 03:17:33 +0200

    feat: update NavMain component to use Link for navigation

--- src/components/nav-main.tsx
+++ src/components/nav-main.tsx
@@ -4,6 +4,7 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from '@ui/sidebar';
+import { Link } from '@tanstack/react-router';
 import type { NavItemType } from '@/types';
 
 export default function NavMain({ items }: { items: Array<NavItemType> }) {
@@ -12,9 +13,15 @@ export default function NavMain({ items }: { items: Array<NavItemType> }) {
       <SidebarMenu>
         {items.map((item) => (
           <SidebarMenuItem key={item.title}>
-            <SidebarMenuButton isActive={item.isActive} tooltip={item.title}>
-              {item.icon && <item.icon />}
-              <span>{item.title}</span>
+            <SidebarMenuButton
+              isActive={item.isActive}
+              tooltip={item.title}
+              asChild
+            >
+              <Link to={item.url}>
+                {item.icon && <item.icon />}
+                <span>{item.title}</span>
+              </Link>
             </SidebarMenuButton>
           </SidebarMenuItem>
         ))}
