23d48e7d7b8acc83266656cd8ce960f90c993721
Peter3Khalil <peter.khalil.fahmy@gmail.com>
2025-12-27 22:17:04 +0200

    feat: add success handler to useLogin mutation to invalidate getMe query

--- src/modules/auth/mutations.ts
+++ src/modules/auth/mutations.ts
@@ -2,9 +2,13 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { login, logout } from './services';
 
 export function useLogin() {
+  const queryClient = useQueryClient();
   return useMutation({
     mutationKey: ['login'],
     mutationFn: login,
+    onSuccess() {
+      queryClient.invalidateQueries({ queryKey: ['getMe'] });
+    },
   });
 }
 
